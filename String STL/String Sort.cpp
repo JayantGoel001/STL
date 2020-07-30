d01cd2138fde90c0b88f40999be1233953f26d7c
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-07-30 22:33:04 +0530

    Add files via upload

---
+++ String STL/String Sort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+bool compare(string a,string b){
+    if(a.length() == b.length()){
+        return a<b;
+    }
+    return a.length()>b.length();
+}
+int main(){
+    int n;
+    cout<<"Enter the value of n:";
+    cin>>n;
+    cin.get();
+
+    string s[n];
+    for(int i=0;i<n;i++){
+        getline(cin,s[i]);
+    }
+    cout<<endl;
+    sort(s,s+n,compare);
+
+    for(int i=0;i<n;i++){
+        cout<<s[i]<<endl;
+    }
+
+}
