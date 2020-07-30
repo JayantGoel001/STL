c7d3be6228f105232c1e83a881a6fb1199a7825b
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-07-30 19:47:50 +0530

    Add files via upload

---
+++ Algorithms STL/More Methods.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 20;
+    swap(a,b);
+    cout<<"A = "<<a<<endl;
+    cout<<"B = "<<b<<endl;
+    int ar[] = {1,2,3,4,6};
+    int n = sizeof(ar)/sizeof(ar[0]);
+    reverse(ar,ar+n);
+    for(int i=0;i<n;i++){
+        cout<<ar[i]<<" ";
+    }
+}
+
