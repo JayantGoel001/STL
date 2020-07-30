80104bfd47e18feab397861754c9dd15b6898c69
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-07-30 17:58:11 +0530

    Add files via upload

--- Algorithms STL/Find Function.cpp
+++ Algorithms STL/Find Function.cpp
@@ -18,4 +18,5 @@ int main(){
     else{
         cout<<it-ar<<endl;
     }
+    
 }
---
+++ Algorithms STL/Money Change Problem.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+bool compare(int a,int b){
+    return a<=b;
+}
+int main(){
+    int coins[] = {1,2,5,10,20,50,100,200,500,2000};
+    int n = sizeof(coins)/sizeof(coins[0]);
+    int money = 168;
+    while(money>0){
+        int lb = lower_bound(coins,coins+n,money,compare)-coins-1;
+        cout<<coins[lb]<<" ";
+        money -= coins[lb];
+    }
+}
+
