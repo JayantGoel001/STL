f04f5abcc30b145937cd853feae5155a040db8e4
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-08-01 03:18:28 +0530

    Add files via upload

---
+++ Stacks & Queue STL/Stack Using Queue.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+template<typename T>
+class Stack{
+public:
+    queue<T> q1;
+    queue<T> q2;
+    void push(T x){
+        q1.push(x);
+    }
+    bool empty(){
+        return q1.empty() && q2.empty();
+    }
+    void pop(){
+        int lenOfq1 = q1.size();
+        for(int i=0;i<lenOfq1-1;i++){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        q1.pop();
+        swap(q1,q2);
+    }
+    int top(){
+        int lenOfq1 = q1.size();
+        for(int i=0;i<lenOfq1-1;i++){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int topEle = q1.front();
+        q2.push(topEle);
+        q1.pop();
+        swap(q1,q2);
+        return topEle;
+    }
+
+};
+int main()
+{
+    Stack<int> s;
+    for(int i=0;i<10;i++){
+        s.push(i);
+    }
+    while(!s.empty()){
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+
+}
+
+
