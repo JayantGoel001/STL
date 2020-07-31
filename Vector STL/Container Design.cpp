39d2029cda2c03d9fb4b599e2def9d1af7ecbd08
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-07-31 23:36:47 +0530

    Add files via upload

--- Vector STL/Container Design.cpp
+++ Vector STL/Container Design.cpp
@@ -2,7 +2,7 @@
 #include "Vector.h"
 using namespace std;
 int main(){
-    Vector v;
+    Vector<int> v;
     v.push_back(1);
     v.push_back(2);
     v.push_back(3);
@@ -13,4 +13,6 @@ int main(){
     for(int i=0;i<v.size();i++){
         cout<<v[i]<<" ";
     }
+
+    cout<<endl<<v.capacity();
 }
--- Vector STL/Vector.h
+++ Vector STL/Vector.h
@@ -1,17 +1,18 @@
+template<typename T>
 class Vector{
     int cs;
     int ms;
-    int *ar;
+    T *ar;
 public:
     Vector(){
         cs = 0;
         ms = 1;
-        ar = new int[ms];
+        ar = new T[ms];
     }
-    void push_back(const int d){
+    void push_back(const T d){
         if(cs==ms){
-            int *oldAr = ar;
-            ar = new int[2*ms];
+            T *oldAr = ar;
+            ar = new T[2*ms];
             ms = ms * 2;
             for(int i=0;i<cs;i++){
                 ar[i] = oldAr[i];
@@ -25,27 +26,27 @@ public:
     void pop_back(){
         cs--;
     }
-    int front() const{
+    T front() const{
         return ar[0];
     }
-    int back()const{
+    T back()const{
         return ar[cs-1];
     }
     bool empty()const{
         return cs==0;
     }
-    int capacity()const{
+    T capacity()const{
         return ms;
     }
-    int at(int i){
+    T at(T i){
         return ar[i];
     }
 
-    int operator[](const int i){
+    T operator[](const T i){
         return ar[i];
     }
 
-    int size() const{
+    T size() const{
         return cs;
     }
 };
