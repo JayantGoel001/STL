f15bd18bb6693b4219ecac480aac05d4041c5e74
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-08-01 02:47:39 +0530

    Add files via upload

---
+++ Stacks & Queue STL/stack.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+    stack<int> s;
+    for(int i=0;i<10;i++){
+        s.push(i);
+    }
+    while(!s.empty()){
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+
+}
