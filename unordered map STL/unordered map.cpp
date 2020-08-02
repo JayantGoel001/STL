84fb7df458578ce24974f4181ea2353514528f71
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-08-02 17:22:56 +0530

    Add files via upload

---
+++ unordered map STL/unordered map.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+using namespace std;
+int main(){
+    unordered_map<string,int> m;
+    m.insert(make_pair("mango",10));
+    m.insert(make_pair("apple",120));
+    m.insert(make_pair("banana",110));
+    m.insert(make_pair("guava",90));
+
+    pair<string,int> p;
+    p.first = "Pineapple";
+    p.second = 120;
+
+    m.insert(p);
+
+    m["lichi"] = 335;
+
+    string key = "guava";
+    auto it = m.find(key);
+    if(it!=m.end()){
+        cout<<"Price of "<<key<<" = "<<m[key];
+    }
+    else{
+        cout<<"Not Found";
+    }
+    cout<<endl;
+    m["guava"] = 40;
+    if(m.count(key)){
+        cout<<"price "<<m[key]<<endl;
+    }
+    else{
+        cout<<"Not Found";
+    }
+
+    m[key] +=22;
+    if(m.count(key)){
+        cout<<"price "<<m[key]<<endl;
+    }
+    else{
+        cout<<"Not Found";
+    }
+
+    m.erase(key);
+    if(m.count(key)){
+        cout<<"price "<<m[key]<<endl;
+    }
+    else{
+        cout<<"Not Found"<<endl;
+    }
+    cout<<endl;
+    m["orange"] = 90;
+    m["Pile"] = 80;
+    for(auto it=m.begin();it!=m.end();it++){
+        cout<<it->first<<"->"<<it->second<<endl;
+    }
+    cout<<endl;
+    for(auto p:m){
+        cout<<p.first<<" : "<<p.second<<endl;
+    }
+}
